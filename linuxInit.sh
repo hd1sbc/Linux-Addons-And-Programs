e77e9c6b549bbe6b028e5342870dfd0445d6a124
Michael Rudin <hd1sbcd2@duck.com>
2024-09-06 15:25:28 -0400

    Update linuxInit.sh link
    Changed the "git clone" link since it was pointing to my private Github account (which is mentioned in detail in the README). I also added a comment to denote that this file hasn't been test much

--- linuxInit.sh
+++ linuxInit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# THIS FILE HAS BEEN MINIMALLY TESTED!!!!
+
 echo "Linux Boot - Adding functionality to your system"
 currDir=$(pwd)
 #creating myScripts file to sending 
@@ -6,7 +8,7 @@ if [ ! -d ~/myScripts ]; then
         mkdir ~/myScripts
 fi
 cd ~/myScripts
-git clone https://github.com/79wwrdsl/Linux-Addons-And-Programs.git
+git clone https://github.com/hd1sbc/Linux-Addons-And-Programs.git
 
 #moving files out of git file into myScripts
 for i in ~/myScripts/Linux-Addons-And-Programs.git/*; do
